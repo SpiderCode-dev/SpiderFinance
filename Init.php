71170166ba146e749e85d4ff6d6e969c38192237
XfirePc <vinygfx@gmail.com>
2023-05-24 22:00:59 -0500

    make first commit for finances

---
+++ Init.php
@@ -0,0 +1,17 @@
+<?php
+namespace FacturaScripts\Plugins\SpiderFinance;
+
+use FacturaScripts\Core\Base\InitClass;
+
+
+class Init extends InitClass
+{
+    public function init()
+    {
+        //$this->loadExtension(new Extension\Controller\ReportResult());
+    }
+    public function update()
+    {
+    }
+
+}
