530456b6f48796a544aed65ddb85dd9647b818cd
XfirePc <vinygfx@gmail.com>
2023-06-06 00:10:24 -0500

    update status register to pending

--- Controller/EditClienteInstalacion.php
+++ Controller/EditClienteInstalacion.php
@@ -4,8 +4,8 @@ namespace FacturaScripts\Plugins\SpiderFinance\Controller;
 
 use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
 use FacturaScripts\Core\Lib\ExtendedController\EditController;
-use FacturaScripts\Dinamic\Model\Cliente;
 use FacturaScripts\Dinamic\Model\Contacto;
+use FacturaScripts\Plugins\SpiderFinance\Model\ClienteInstalacion;
 
 class EditClienteInstalacion extends EditController
 {
@@ -28,7 +28,66 @@ class EditClienteInstalacion extends EditController
         $this->createRecurrentView();
         $this->createRecursosView();
         $this->createLineasProgramadasView();
+        $this->showButtons();
     }
+
+    public function execPreviousAction($action)
+    {
+        if ($action == 'setup-install') {
+            $model = $this->getModel();
+            $model->loadFromCode($this->request->query->get('code'));
+            if ($model->status != ClienteInstalacion::STATUS_REGISTER) {
+                $this->toolBox()->i18nLog()->warning('Instalación ya configurada.');
+                return;
+            }
+
+            $data = $this->request->request->all();
+            $this->dataBase->beginTransaction();
+            $created = $model->setupInstall($data);
+
+            if (!$created) {
+                $this->toolBox()->i18nLog()->warning('No se ha podido crear la instalación.');
+                return;
+            }
+
+            $this->dataBase->commit();
+            $this->toolBox()->i18nLog()->info('Instalación iniciada correctamente, configure los datos de mikrotik.');
+        }
+
+        if ($action == 'pending-install') {
+            $model = $this->getModel();
+            $model->loadFromCode($this->request->query->get('code'));
+            $model->status = ClienteInstalacion::STATUS_PENDING;
+            $model->save();
+        }
+
+        return parent::execPreviousAction($action);
+    }
+
+
+    public function execAfterAction($action)
+    {
+        parent::execAfterAction($action);
+
+        if ($action == 'insert' && $this->active == 'EditClienteInstalacion') {
+            $model = $this->getModel();
+            $data = $this->request->request->all();
+            $this->dataBase->beginTransaction();
+            $created = $model->init($data);
+
+            if (!$created) {
+                $model->delete();
+                $this->dataBase->rollback();
+                $this->toolBox()->i18nLog()->warning('No se ha podido crear la instalación.');
+                return;
+            }
+
+            $this->dataBase->commit();
+            $this->loadData($this->getMainViewName(), $this->views[$this->getMainViewName()]);
+            $this->toolBox()->i18nLog()->info('Instalación creada correctamente, configure los documentos programados.');
+        }
+    }
+
 
     public function createRecurrentView($viewName = 'ListDocRecurringSale') {
         $this->addListView($viewName, 'DocRecurringSale', 'recurring', 'fas fa-calendar-plus');
@@ -52,24 +111,55 @@ class EditClienteInstalacion extends EditController
         $mainModel = $this->getModel();
 
         if ($viewName == $this->getMainViewName())
-            if ($mainModel->exists()) {
+            if ($mainModel->exists()  && $mainModel->idcontacto) {
                 $contact = (new Contacto())->get($mainModel->idcontacto);
-                $client = (new Cliente())->get($contact->codcliente);
+                $client = $contact->getCustomer();
 
                 $mainModel->cifnif = $client->cifnif;
                 $mainModel->nombrecliente = $client->razonsocial;
                 $mainModel->email = $contact->email;
                 $mainModel->telefono = $contact->telefono1;
                 $mainModel->direccion = $contact->direccion;
+
+                // TODO: Load Recurring doc
+                // TODO: Edit data update contact
+
             }
 
         if ($viewName == 'ListLineaProgramada') {
             $where = [new DataBaseWhere('id_installation', $mainModel->primaryColumnValue())];
             $view->loadData('', $where);
         }
+
+        if ($viewName == 'ListDocRecurringSale') {
+            $where = [new DataBaseWhere('id_installation', $mainModel->id)];
+            $view->loadData('', $where);
+        }
     }
 
+    public function showButtons() {
+        $mvn = $this->getMainViewName();
+        $model = $this->getModel();
+        $model->loadFromCode($this->request->query->get('code'));
+        if ($model->exists()) {
+            if ($model->status == ClienteInstalacion::STATUS_REGISTER)
+                $this->addButton($mvn, [
+                    'action' => 'pending-install',
+                    'icon' => 'fas fa-check',
+                    'label' => 'Confirmar instalación',
+                    'type' => 'action',
+                    'color' => 'warning'
+                ]);
+            if ($model->status == ClienteInstalacion::STATUS_PENDING)
+                $this->addButton($mvn, [
+                    'action' => 'setup-install',
+                    'icon' => 'fas fa-network-wired',
+                    'label' => 'Iniciar instalación',
+                    'type' => 'action',
+                    'confirm' => true,
+                    'color' => 'success'
+                ]);
+        }
 
-
-
+    }
 }
--- Model/ClienteInstalacion.php
+++ Model/ClienteInstalacion.php
@@ -2,8 +2,14 @@
 
 namespace FacturaScripts\Plugins\SpiderFinance\Model;
 
+use FacturaScripts\Core\App\AppSettings;
+use FacturaScripts\Core\Base\ToolBox;
 use FacturaScripts\Core\Model\Base\ModelClass;
 use FacturaScripts\Core\Model\Base\ModelTrait;
+use FacturaScripts\Dinamic\Model\Cliente;
+use FacturaScripts\Dinamic\Model\Contacto;
+use FacturaScripts\Dinamic\Model\DocRecurringSaleLine;
+use FacturaScripts\Plugins\DocumentosRecurrentes\Model\DocRecurringSale;
 
 class ClienteInstalacion extends ModelClass
 {
@@ -15,6 +21,17 @@ class ClienteInstalacion extends ModelClass
     const STATUS_PENDING = 1;
     const STATUS_INSTALLED = 2;
     const STATUS_CANCELLED = 3;
+
+    public function __construct(array $data = [])
+    {
+        parent::__construct($data);
+        if (!$this->exists()) {
+            $this->status = self::STATUS_REGISTER;
+            $this->codpago = AppSettings::get('default', 'codpago');
+            $this->codserie = AppSettings::get('default', 'codserie');
+            $this->codalmacen = AppSettings::get('default', 'codalmacen');
+        }
+    }
 
     public function __get($name)
     {
@@ -56,5 +73,88 @@ class ClienteInstalacion extends ModelClass
             default:
                 return 'Desconocido';
         }
+    }
+
+    public function setupInstall($data) {
+        if (!$this->exists()) {
+            ToolBox::log()->warning('La instalación no existe, guarde los datos para ejecutar esta acción.');
+            return false;
+        }
+
+        $this->loadFromData($data);
+        if (!$this->save()) {
+            return false;
+        }
+
+        //Use customer
+        $this->status = self::STATUS_PENDING;
+        $customer = $this->getCustomer();
+        if (!$customer) {
+            return false;
+        }
+
+        //Create recurring doc
+        $doc = new DocRecurringSale();
+        $doc->codpago = $data['codpago'];
+        $doc->codserie = $data['codserie'];
+        $doc->codalmacen = $data['codalmacen'];
+        $doc->startdate = date('Y-m-d');
+        $doc->coddivisa = AppSettings::get('default', 'coddivisa');
+        $doc->codcliente = $customer->codcliente;
+        $doc->generatedoc = 'FacturaCliente';
+        $doc->name = "Instalación " . $this->id;
+        $doc->id_installation = $this->id;
+        if (!$doc->save()) {
+            return false;
+        }
+
+        //Create recurring line doc with plan value
+        $plan = new Plan();
+        $plan->loadFromCode($data['idplan']);
+        $product = $plan->getProduct();
+
+        $line = new DocRecurringSaleLine();
+        $line->iddoc = $doc->id;
+        $line->idproduct = $product->idproducto;
+        $line->reference = $product->referencia;
+        $line->quantity = 1;
+        $line->price = $plan->price;
+        if (!$line->save()) {
+            return false;
+        }
+
+        return $this->save();
+    }
+
+    public function init($data)
+    {
+        //Create contact
+        $contacto = new Contacto();
+        $contacto->nombre = $data['nombrecliente'];
+        $contacto->cifnif = $data['cifnif'];
+        $contacto->telefono1 = $data['telefono'];
+        $contacto->email = $data['email'];
+        $contacto->direccion = $data['direccion'];
+        if (!$contacto->save()) {
+            return false;
+        }
+
+        //Create customer
+        $customer = $contacto->getCustomer(true);
+        if (!$customer->save()) {
+            return false;
+        }
+
+        $this->codcliente = $customer->codcliente;
+        $this->idcontacto = $contacto->idcontacto;
+
+        return $this->save();
+    }
+
+    public function getCustomer()
+    {
+        $customer = new Cliente();
+        $customer->loadFromCode($this->codcliente);
+        return $customer->codcliente ? $customer : null;
     }
 }
